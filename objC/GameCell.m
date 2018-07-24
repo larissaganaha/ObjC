6547a6cdf2d0d78bbebd447c3ae58dc95c626a19
Larissa Ganaha <larissaganaha@gmail.com>
2018-07-24 16:01:58 -0300

    first commit

---
+++ objC/GameCell.m
@@ -0,0 +1,24 @@
+//
+//  GameCell.m
+//  objC
+//
+//  Created by Larissa Ganaha on 24/07/18.
+//  Copyright © 2018 Larissa Ganaha. All rights reserved.
+//
+
+#import "GameCell.h"
+
+@implementation GameCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
